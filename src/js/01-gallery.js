63a0398a95b943ec2d9f52f3f2b450344a4ddea1
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-03-30 21:58:46 +0300

    work with HW

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  galleryEl: document.querySelector('.gallery'),
+};
+
+const modalGallery = new SimpleLightbox('.gallery a');
+
+const readyGalleryContent = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></li>`;
+  })
+  .join('');
+
+refs.galleryEl.innerHTML = readyGalleryContent;
+refs.galleryEl.addEventListener('click', disableHrefAndShowModal);
+
+function disableHrefAndShowModal(event) {
+  event.preventDefault();
+  console.log(modalGallery);
+  modalGallery.show();
+  const { source } = event.target.dataset;
+  const { alt } = event.target;
+
+  //   createModal(source, alt);
+}
+
+// function createModal(source, alt) {
+//   console.log(modalGallery.on('show.simplelightbox'));
+// }
