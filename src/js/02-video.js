d1886300a166408600a1dd8ab1c993ae38bae170
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-04-04 15:36:11 +0300

    cancel usage of object, smallest code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,13 +2,10 @@ import VimeoPlayer from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const iframeEl = document.querySelector('#vimeo-player');
 const player = new VimeoPlayer(iframeEl);
-const savedTime = {
-  mySeconds: 0,
-};
-const saveData = (key, value) => {
+const saveData = ({ seconds }) => {
   try {
-    const data = JSON.stringify(value);
-    localStorage.setItem(key, data);
+    const data = JSON.stringify(seconds);
+    localStorage.setItem('videoplayer-current-time', data);
   } catch (err) {
     console.error('Stringify error:', err.message, err.type);
   }
@@ -16,18 +13,39 @@ const saveData = (key, value) => {
 const readData = key => {
   try {
     const data = localStorage.getItem(key);
-    return data === null ? undefined : JSON.parse(data);
+    return data === null ? 0 : JSON.parse(data);
   } catch (err) {
     console.error('Parse error:', err.message, err.type);
   }
 };
 
-player.on(
-  'timeupdate',
-  throttle(({ seconds }) => {
-    savedTime.mySeconds = seconds;
-    saveData('videoplayer-current-time', savedTime.mySeconds);
-  }, 1000)
-);
+player.on('timeupdate', throttle(saveData, 1000));
 
 player.setCurrentTime(readData('videoplayer-current-time'));
+
+// const saveData = (key, value) => {
+//   try {
+//     const data = JSON.stringify(value);
+//     localStorage.setItem(key, data);
+//   } catch (err) {
+//     console.error('Stringify error:', err.message, err.type);
+//   }
+// };
+// const readData = key => {
+//   try {
+//     const data = localStorage.getItem(key);
+//     return data === null ? undefined : JSON.parse(data);
+//   } catch (err) {
+//     console.error('Parse error:', err.message, err.type);
+//   }
+// };
+
+// player.on(
+//   'timeupdate',
+//   throttle(({ seconds }) => {
+//     savedTime.mySeconds = seconds;
+//     saveData('videoplayer-current-time', savedTime.mySeconds);
+//   }, 1000)
+// );
+
+// player.setCurrentTime(readData('videoplayer-current-time'));
